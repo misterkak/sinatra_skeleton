f24b1888b079212e8b6e385f9c7c780013012064
misterkak <mike.kakebeeke@gmail.com>
2014-09-21 11:59:36 -0700

    all exercises up to but not including 12

--- app/actions.rb
+++ app/actions.rb
@@ -8,3 +8,28 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+get '/messages/new' do
+  @message = Message.new
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+#binding.pry
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
